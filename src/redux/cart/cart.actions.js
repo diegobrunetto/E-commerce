020e29d97910aa8cedb823603e649364c6887bf2
diegobrunetto <diego.brunetto@hotmail.com>
2020-04-25 17:35:31 -0300

    update shop data and group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,7 +2,7 @@ import CartActionTypes from './cart.types.js';
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
-});
+})
 
 export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
-import CartActionTypes from './cart.types';  
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -9,13 +10,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
-        ...state, 
+        ...state,
         hidden: !state.hidden
       };
     case CartActionTypes.ADD_ITEM:
-      return{
+      return {
         ...state,
-        cartItems: [...state.cartItems, action.payload]
+        cartItems: addItemToCart(state.cartItems, action.payload)
       }
     default:
       return state

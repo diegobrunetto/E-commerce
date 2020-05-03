2b53453ace1db6a4e85bf4ebe60f6a7102c9ac15
diegobrunetto <diego.brunetto@hotmail.com>
2020-05-02 21:10:09 -0300

    create collection page for every category and his routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import { selectCollections } from '../../redux/shop/shop.selector';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    {
-      collections.map(({ id, ...otherCollectionsProps }) => (
-        <CollectionPreview key={id} {...otherCollectionsProps} />
-      ))
-    }
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
